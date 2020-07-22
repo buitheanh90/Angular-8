9dd39e5fa65f513cbd831654a79769b3b00781c9
AnhBui <buitheanh1990@gmail.com>
2020-07-22 23:15:25 +0700

    config code

--- src/app/service/product.service.ts
+++ src/app/service/product.service.ts
@@ -4,16 +4,16 @@ import { Product } from "../model/product.class";
 import { Observable } from "rxjs";
 
 const httpOptions = {
-  headers: new HttpHeaders().set("Content-Type", "x-www-form-urlencoded"),
+	headers: new HttpHeaders().set("Content-Type", "x-www-form-urlencoded"),
 };
 
 @Injectable()
 export class ProductService {
-  API: string = "/api/products";
+	API: string = "/api/products";
 
-  constructor(private http: HttpClient) {}
+	constructor(private http: HttpClient) {}
 
-  getProducts(): Observable<Product[]> {
-    return this.http.post<Product[]>(this.API, httpOptions);
-  }
+	getProducts(): Observable<Product[]> {
+		return this.http.post<Product[]>(this.API, httpOptions);
+	}
 }
