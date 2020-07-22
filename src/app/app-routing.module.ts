2ebbc52e3f613b8f363e07af0cf0716da3f9cfe3
AnhBui <buitheanh1990@gmail.com>
2020-07-22 13:12:54 +0700

    Edit source component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { HomeComponent } from "./components/pages/home/home.component";
+import { SignupComponent } from "./components/pages/signup/signup.component";
 
 const routes: Routes = [
 	{
 		path: "",
 		component: HomeComponent,
+	},
+	{
+		path: "signup",
+		component: SignupComponent,
 	},
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,22 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { AppRoutingModule } from "./app-routing.module";
 import { HttpClientModule } from "@angular/common/http";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
+//components
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./components/layout/header/header.component";
 import { HomeComponent } from "./components/pages/home/home.component";
 import { FooterComponent } from "./components/layout/footer/footer.component";
+import { SignupComponent } from "./components/pages/signup/signup.component";
+import { ValidateComponent } from "./components/pages/validate/validate.component";
 
+//services
 import { ProductService } from "./service/product.service";
 
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+//material
 import { MatTabsModule } from "@angular/material";
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
 @NgModule({
 	declarations: [
@@ -20,6 +25,8 @@ import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 		HeaderComponent,
 		HomeComponent,
 		FooterComponent,
+		SignupComponent,
+		ValidateComponent,
 	],
 	imports: [
 		BrowserModule,
